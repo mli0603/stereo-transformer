ad9ca4fc0a45ac81dfae81646517ffeea9f3856d
Max Zhaoshuo Li <zli122@jhu.edu>
2020-11-12 15:51:17 -0500

    removed copyright statement

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#
-# Author: Zhaoshuo Li, Xingtong Liu, Francis X. Creighton, Russell H. Taylor, and Mathias Unberath
-#
-# Copyright (c) 2020. Johns Hopkins University - All rights reserved.
-#
-
 CUDA_VISIBLE_DEVICES=0
 python main.py  --epochs 15\
                 --batch_size 1\
